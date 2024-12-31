3623a0267ae5e37f79ce6c6f0c3eec2aa1533bfc
muslimumarov <muslimbekaxmatovich@gmail.com>
2024-12-31 12:49:49 +0500

    ...dkfjlfk

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 "use strict";
-function add(a, b) {
-    return a ** b;
+// function  add(a:number, b:number):number {
+//   return a** b
+// }
+// console.log(add(4, 5));
+// function pow(a:number, b: number): string {
+//   return `${a ** b} - ${a + b}`
+// }
+// console.log(pow(3 , 5));
+function pow(x, y) {
+    if (typeof x === "number" && typeof y === "number") {
+        return x + y;
+    }
+    else {
+        return `${x} ${y}`;
+    }
 }
-console.log(add(4, 5));
+console.log(pow('slsls', 'ldkfjdf'));
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,4 +1,17 @@
-function  add(a:number, b:number):number {
-  return a** b
+// function  add(a:number, b:number):number {
+//   return a** b
+// }
+// console.log(add(4, 5));
+// function pow(a:number, b: number): string {
+//   return `${a ** b} - ${a + b}`
+// }
+// console.log(pow(3 , 5));
+
+function pow(x: any, y: any): any {
+   if(typeof x === "number" && typeof y === "number") {
+    return x + y
+  } else {
+    return `${x} ${y}`
+  }
 }
-console.log(add(4, 5));
+console.log(pow('slsls', 'ldkfjdf'));
