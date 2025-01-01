a1b2b7f0d924dcb3f63217c35d39e8ef89453aeb
muslimumarov <muslimbekaxmatovich@gmail.com>
2025-01-01 09:27:27 +0500

    ..defkf

--- index.js
+++ index.js
@@ -1,34 +1,37 @@
 "use strict";
-// function  add(a:number, b:number):number {
-//   return a** b
+// // function  add(a:number, b:number):number {
+// //   return a** b
+// // }
+// // console.log(add(4, 5));
+// // function pow(a:number, b: number): string {
+// //   return `${a ** b} - ${a + b}`
+// // }
+// // console.log(pow(3 , 5));
+// // function pow(x: any, y: any): any {
+// //    if(typeof x === "number" && typeof y === "number") {
+// //     return x + y
+// //   } else {
+// //     return `${x} ${y}`
+// //   }
+// // }
+// // console.log(pow(true, false));
+// let c: unknown = 20
+// let d: number = <number>c
+// let b: number = c as number
+// console.log(b);
+// let w: unknown = 20; // Hozircha `c` ma'lum emas.
+// console.log(c); // Ishlaydi: 20
+// c = "Hello"; // O'zgartirish mumkin.
+// console.log(w); // Ishlaydi: Hello
+// let g: unknown = 'ldkfkdfj'
+// if(typeof g === 'number') {
+//        let a =  g *g
+//    console.log(a);
+// }else {
+//   console.log('malumot turi xato');
 // }
-// console.log(add(4, 5));
-// function pow(a:number, b: number): string {
-//   return `${a ** b} - ${a + b}`
-// }
-// console.log(pow(3 , 5));
-// function pow(x: any, y: any): any {
-//    if(typeof x === "number" && typeof y === "number") {
-//     return x + y
-//   } else {
-//     return `${x} ${y}`
-//   }
-// }
-// console.log(pow(true, false));
-let c = 20;
-let d = c;
-let b = c;
-console.log(b);
-let w = 20; // Hozircha `c` ma'lum emas.
-console.log(c); // Ishlaydi: 20 
-c = "Hello"; // O'zgartirish mumkin.
-console.log(w); // Ishlaydi: Hello
-let g = 'ldkfkdfj';
-if (typeof g === 'number') {
-    let a = g * g;
-    console.log(a);
-}
-else {
-    console.log('malumot turi xato');
-}
+let a = [23, 'so', true];
+console.log(a);
+let c = [34];
+c = [34, { ket: 3, bor: 44, }];
 //# sourceMappingURL=index.js.map
